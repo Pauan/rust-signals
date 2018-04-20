5ccdbb916ab6d916ca339cce219ddf501bf6747f
Pauan <pcxunlimited@gmail.com>
2018-04-19 17:33:52 -1000

    Reorganizing into a separate mutable.rs file

--- src/signal/mod.rs
+++ src/signal/mod.rs
@@ -1,6 +1,8 @@
+mod broadcaster;
+pub use self::broadcaster::*;
+
+mod mutable;
+pub use self::mutable::*;
 
 mod signal;
 pub use self::signal::*;
-
-mod broadcaster;
-pub use self::broadcaster::*;
