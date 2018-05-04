3d3ccf41c97779c4e8c12faa787829daf3503ae0
Pauan <pcxunlimited@gmail.com>
2018-05-03 23:58:10 -1000

    Refactoring Broadcaster

--- src/signal/broadcaster.rs
+++ src/signal/broadcaster.rs
@@ -6,14 +6,14 @@ use futures_core::task::{Context, Waker, Wake};
 
 
 struct BroadcasterStatus {
-    has_changed: AtomicBool,
+    is_changed: AtomicBool,
     waker: Mutex<Option<Waker>>,
 }
 
 impl BroadcasterStatus {
     fn new() -> Self {
         Self {
-            has_changed: AtomicBool::new(true),
+            is_changed: AtomicBool::new(true),
             waker: Mutex::new(None),
         }
     }
@@ -24,14 +24,14 @@ impl BroadcasterStatus {
 /// This is responsible for propagating a "wake" down to any pending tasks
 /// attached to broadcasted children.
 struct BroadcasterNotifier {
-    is_waiting: AtomicBool,
+    is_changed: AtomicBool,
     targets: Mutex<Vec<Weak<BroadcasterStatus>>>,
 }
 
 impl BroadcasterNotifier {
     fn new() -> Self {
         Self {
-            is_waiting: AtomicBool::new(false),
+            is_changed: AtomicBool::new(true),
             targets: Mutex::new(vec![]),
         }
     }
@@ -39,13 +39,17 @@ impl BroadcasterNotifier {
     fn notify(&self, is_changed: bool) {
         let mut lock = self.targets.lock().unwrap();
 
+        if is_changed {
+            self.is_changed.store(true, Ordering::SeqCst);
+        }
+
         // Take this opportunity to GC dead children
         lock.retain(|weak_child_state| {
             if let Some(child_status) = weak_child_state.upgrade() {
                 let mut lock = child_status.waker.lock().unwrap();
 
                 if is_changed {
-                    child_status.has_changed.store(true, Ordering::SeqCst);
+                    child_status.is_changed.store(true, Ordering::SeqCst);
                 }
 
                 if let Some(waker) = lock.take() {
@@ -59,8 +63,6 @@ impl BroadcasterNotifier {
                 false
             }
         });
-
-        self.is_waiting.store(false, Ordering::SeqCst);
     }
 }
 
@@ -73,40 +75,75 @@ impl Wake for BroadcasterNotifier {
 
 // ---------------------------------------------------------------------------
 
+struct BroadcasterInnerState<A> where A: Signal {
+    signal: A,
+    value: Option<A::Item>,
+}
+
+impl<A> BroadcasterInnerState<A> where A: Signal {
+    fn new(signal: A) -> Self {
+        Self {
+            signal,
+            value: None,
+        }
+    }
+
+    // Poll the underlying signal for changes, giving it a BroadcasterNotifier
+    // to wake in the future if it is in Pending state.
+    fn poll_underlying(&mut self, cx: &mut Context, notifier: Arc<BroadcasterNotifier>) {
+        let waker = Waker::from(notifier);
+
+        let mut sub_cx = cx.with_waker(&waker);
+
+        loop {
+            // TODO what if it is woken up while polling ?
+            match self.signal.poll_change(&mut sub_cx) {
+                Async::Ready(value) => {
+                    let done = value.is_none();
+
+                    self.value = value;
+
+                    if done {
+                        break;
+                    }
+                },
+                Async::Pending => {
+                    break;
+                },
+            }
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------
+
 // Shared state underpinning a Cloned set of broadcasters
 struct BroadcasterSharedState<A> where A: Signal {
-    signal: Mutex<A>,
-    value: RwLock<Option<A::Item>>,
+    inner: RwLock<BroadcasterInnerState<A>>,
     notifier: Arc<BroadcasterNotifier>,
 }
 
 impl<A> BroadcasterSharedState<A> where A: Signal {
     fn new(signal: A) -> Self {
         Self {
-            signal: Mutex::new(signal),
-            value: RwLock::new(None),
+            inner: RwLock::new(BroadcasterInnerState::new(signal)),
             notifier: Arc::new(BroadcasterNotifier::new()),
         }
     }
 
-    // Poll the underlying signal for changes, giving it a BroadcasterNotifier
-    // to wake in the future if it is in Pending state.
-    //
-    // If the underlying `Signal` generates `Async::Ready`, then all the
-    // `BroadcasterSignal`s in the group will have their `has_changed` flag
-    // set so they can pick up the new value with their next poll.
-    fn poll_underlying(&self, cx: &mut Context) {
-        // Set the notifier to be waiting on a poll, and if it wasn't
-        // previously, actually execute that poll.
-        if !self.notifier.is_waiting.swap(true, Ordering::SeqCst) {
-            let waker = Waker::from(self.notifier.clone());
-            let mut sub_cx = cx.with_waker(&waker);
-            let change = self.signal.lock().unwrap().poll_change(&mut sub_cx);
+    fn poll<B, F>(&self, cx: &mut Context, f: F) -> B where F: FnOnce(&Option<A::Item>) -> B {
+        // TODO is this correct ?
+        if self.notifier.is_changed.swap(false, Ordering::SeqCst) {
+            let mut lock = self.inner.write().unwrap();
 
-            if let Async::Ready(value) = change {
-                *self.value.write().unwrap() = value;
-                self.notifier.notify(true);
-            }
+            lock.poll_underlying(cx, self.notifier.clone());
+
+            f(&lock.value)
+
+        } else {
+            let lock = self.inner.read().unwrap();
+
+            f(&lock.value)
         }
     }
 }
@@ -138,17 +175,11 @@ impl<A> BroadcasterState<A> where A: Signal {
     }
 
     fn poll_change<F>(&self, cx: &mut Context, f: F) -> Async<Option<A::Item>> where F: FnOnce(&Option<A::Item>) -> Option<A::Item> {
-        // Don't need any potential waker as this task is now awake!
-        *self.status.waker.lock().unwrap() = None;
-
-        // Check for changes in the underlying signal (if not waiting already).
-        self.shared_state.poll_underlying(cx);
-
         // If the poll just done (or a previous poll) has generated a new
         // value, we can report it. Use swap so only one thread will pick up
         // the change
-        if self.status.has_changed.swap(false, Ordering::SeqCst) {
-            Async::Ready(f(&self.shared_state.value.read().unwrap()))
+        if self.status.is_changed.swap(false, Ordering::SeqCst) {
+            Async::Ready(self.shared_state.poll(cx, f))
 
         } else {
             // Nothing new to report, save this task's Waker for later
