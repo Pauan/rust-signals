67eaa47d912d30859fde3a57cb64ce58ee6a7a6c
Keaton Brandt <keaton.brandt@gmail.com>
2020-12-23 16:50:48 -0800

    Adds SignalMapExt.watch_key
    Also adds Copy methods to BTreeMap, and adds some unit tests for SignalMap.

---
+++ tests/signal_map.rs
@@ -0,0 +1,118 @@
+use std::task::Poll;
+use futures_signals::signal_map::{MapDiff, SignalMapExt};
+
+mod util;
+
+#[test]
+fn map_value() {
+    let input = util::Source::new(vec![
+        Poll::Ready(MapDiff::Replace {
+            entries: vec![(1, 1), (2, 1), (3, 2), (4, 3)]
+        }),
+        Poll::Pending,
+        Poll::Ready(MapDiff::Insert {
+            key: 5,
+            value: 5,
+        }),
+        Poll::Ready(MapDiff::Update {
+            key: 1,
+            value: 0,
+        }),
+        Poll::Pending,
+        Poll::Ready(MapDiff::Remove {key: 1}),
+        Poll::Ready(MapDiff::Insert {
+            key: 1,
+            value: 1,
+        }),
+        Poll::Ready(MapDiff::Clear {})
+    ]);
+
+    let output = input.map_value(|value| value * 2);
+
+    util::assert_signal_map_eq(output, vec![
+        Poll::Ready(Some(MapDiff::Replace {
+            entries: vec![(1, 2), (2, 2), (3, 4), (4, 6)]
+        })),
+        Poll::Pending,
+        Poll::Ready(Some(MapDiff::Insert {
+            key: 5,
+            value: 10,
+        })),
+        Poll::Ready(Some(MapDiff::Update {
+            key: 1,
+            value: 0,
+        })),
+        Poll::Pending,
+        Poll::Ready(Some(MapDiff::Remove {key: 1})),
+        Poll::Ready(Some(MapDiff::Insert {
+            key: 1,
+            value: 2,
+        })),
+        Poll::Ready(Some(MapDiff::Clear {})),
+        Poll::Ready(None),
+    ]);
+}
+
+#[test]
+fn watch_key_exists_at_start() {
+    let input = util::Source::new(vec![
+        Poll::Ready(MapDiff::Replace {
+            entries: vec![(1, 1), (2, 1), (3, 2), (4, 3)]
+        }),
+        Poll::Pending,
+        Poll::Ready(MapDiff::Update {
+            key: 1,
+            value: 0,
+        }),
+        Poll::Ready(MapDiff::Update {
+            key: 2,
+            value: 0,
+        }),
+        Poll::Pending,
+        Poll::Ready(MapDiff::Remove {key: 1})
+    ]);
+
+    let output = input.watch_key(1);
+
+    util::assert_signal_eq(output, vec![
+        Poll::Ready(Some(Some(1))),
+        Poll::Pending,
+        Poll::Ready(Some(Some(0))),
+        Poll::Pending,
+        Poll::Pending,
+        Poll::Ready(Some(None)),
+        Poll::Ready(None),
+    ]);
+}
+
+#[test]
+fn watch_key_does_not_exist_at_start() {
+    let input = util::Source::new(vec![
+        Poll::Ready(MapDiff::Replace {
+            entries: vec![(1, 1), (2, 1), (3, 2), (4, 3)]
+        }),
+        Poll::Pending,
+        Poll::Ready(MapDiff::Insert {
+            key: 5,
+            value: 5,
+        }),
+        Poll::Ready(MapDiff::Update {
+            key: 5,
+            value: 0,
+        }),
+        Poll::Pending,
+        Poll::Ready(MapDiff::Clear {})
+    ]);
+
+    let output = input.watch_key(5);
+
+    util::assert_signal_eq(output, vec![
+        Poll::Ready(Some(None)),
+        Poll::Pending,
+        Poll::Ready(Some(Some(5))),
+        Poll::Ready(Some(Some(0))),
+        Poll::Pending,
+        Poll::Ready(Some(None)),
+        Poll::Ready(None),
+    ]);
+}
